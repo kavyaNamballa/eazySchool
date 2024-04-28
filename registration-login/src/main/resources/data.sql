6af3a3b7ecc3292c9628728bdc1939b4397d6816
kavyaNamballa <163406073+kavyaNamballa@users.noreply.github.com>
2024-04-28 21:00:32 +0530

    Delete registration-login directory

--- registration-login/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
-  VALUES ('ADMIN',CURDATE(),'DBA');
-
-INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
-  VALUES ('STUDENT',CURDATE(),'DBA');
